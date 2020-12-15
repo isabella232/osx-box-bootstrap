631ee2e6126e9bd1136fcddcba6d2b74e71da1f3
fehersanyi-bitrise <sandor.feher@bitrise.io>
2020-12-15 10:13:53 +0100

    [IN-2297] Unshallow hombrew core local mirror

--- roles/brew-repos-fix/files/setup_brew_core.sh
+++ roles/brew-repos-fix/files/setup_brew_core.sh
@@ -5,15 +5,18 @@ set -e
 source ~/.bash_profile
 
 brew_repository=$(brew --repository)
+local_mirror=~/mirrors/github.com/bitrise-io/homebrew-core
 
 rm -rf "$brew_repository/Library/Taps/homebrew"
 
-rm -rf ~/mirrors/github.com/bitrise-io/homebrew-core
+rm -rf "${local_mirror}"
 
 mkdir -p ~/mirrors/github.com/bitrise-io/
 
-git clone --bare https://github.com/bitrise-io/homebrew-core ~/mirrors/github.com/bitrise-io/homebrew-core
+git clone --bare https://github.com/bitrise-io/homebrew-core "${local_mirror}"
 
-export HOMEBREW_CORE_GIT_REMOTE="~/mirrors/github.com/bitrise-io/homebrew-core"
+git -C "${local_mirror}" fetch --unshallow
+
+export HOMEBREW_CORE_GIT_REMOTE="${local_mirror}"
 
 brew install a || true
